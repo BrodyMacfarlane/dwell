267e4fa56d847e1e87149e693a7e31fa52e21bee
Brody Macfarlane <macfarlanebrody@gmail.com>
2018-01-30 02:49:11 -0700

    minor aesthetic changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import Animation from './components/animation'
+import './css/animation.css'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      isAnimating: true
+    }
+  }
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({isAnimating: false})
+    }, 9000);
+  }
   render() {
     return (
       <div className="App">
-        <Animation/>
+        <div className={`animation-component ${this.state.isAnimating ? null : 'goine'}`}>
+          <Animation/>
+        </div>
+        <div>WOOOOOOOOO ACTUAL SITE WILL GO HERE</div>
       </div>
     );
   }
